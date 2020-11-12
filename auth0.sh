76bdd8e03ef0f846d60e9922c23b962762f30d44
mgrybel <monika.grybel@hotmail.com>
2020-11-12 07:14:48 +0000

    Added Auth details

--- auth0.sh
+++ auth0.sh
@@ -1,4 +1,3 @@
 export AUTH0_DOMAIN='fsnd-casting-agency.eu.auth0.com'
 export AUTH0_CLIENT_ID='l5buZVd3KWO4wghf7S5RvHZqZZtoU0hy'
 export AUTH0_AUDIENCE='agency'
-export JWT_SECRET='jafaREDagr23432fvsf'
